5197ec51852eed678efd6f630a5bb3a0b1292c1b
Sneha Kumari <43563928+snehaKums@users.noreply.github.com>
2021-07-23 11:53:19 +0530

    Update Product.js

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -15,8 +15,8 @@ export default function Product({postNum,search}){
                                 <Image
                                     alt='Logo img'
                                     src={item.image}
-                                    width={200}
-                                    height={200}
+                                    width={180}
+                                    height={180}
                                     />
                                     <h1 className={styles.cardText}>{item.title}</h1>
                                     <p className={styles.cardSubtext}>Price: $ {item.price}</p>
